5ff0c32e458ecf58f3dc6d7508be94ad3dc88028
3364fy <2050396327@qq.com>
2022-04-15 20:41:11 +0800

    project

--- 自然语言处理/pytorch.py
+++ 自然语言处理/pytorch.py
@@ -4,6 +4,7 @@ import torch
 x=torch.empty(5,3)
 print(x)
 #高斯分布，均值为零，方差为一
+#初始化5行三列的张量
 x2=torch.rand(5,3)
 print(x2)
 
@@ -35,7 +36,22 @@ print(y)
 x.copy_(y)
 print(x)
 
+print(x[:,1])
 
+x=torch.rand(4,4)
+#torch.view()操作需要保证数据元素的总数量不变
+y=x.view(16)
+#-1代表自动匹配个数
+z=x.view(-1,8)
+print(x.size(),y.size(),z.size())
+
+#如果张量中只有一个元素，可以用.item()将值取出，作为一个python number
+x=torch.rand(1)
+print(x)
+print(x.item())
+
+a=torch.ones(5)
+print(a)
 
 
 
