d1ca4793b3c9a000280f7671447b55c6835fb630
3364fy <2050396327@qq.com>
2022-04-15 17:42:53 +0800

    project

---
+++ NLP_/1.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import torch
+
+x=torch.empty(5,3)
+print(x)
+#高斯分布，均值为零，方差为一
+x2=torch.rand(5,3)
+print(x2)
+
+x3=torch.zeros(5,3,dtype=torch.int)
+print(x3)
+
+x4=torch.tensor([2.5,3.5])
+print(x4)
+
+x=x.new_ones(5,3,dtype=torch.double)
+print(x,x.size())
+y=torch.randn_like(x,dtype=torch.float)
+print(y)
